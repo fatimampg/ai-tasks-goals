5e6f71a3b1d96a075630cd0b0cf2a675f8e7a7c5
fatimampg <fatimampgouveia@gmail.com>
2024-04-23 01:02:03 +0100

    style: update style and change color of button SAVE CHANGES when changes are made

--- frontend/src/pages/Tasks.tsx
+++ frontend/src/pages/Tasks.tsx
@@ -56,12 +56,43 @@ const Tasks = () => {
   const handleUpdateTasksTopParent = (updatedNewTaskList: Task[]) => {
     setUpdatedTaskStatusList(updatedNewTaskList);
   };
-  // useEffect(() => {
-  //   console.log(
-  //     "Task List with updated status (from Parent: Tasks.tsx) - SEND TO THE DATABASE:",
-  //     updatedTaskStatusList,
-  //   );
-  // }, [updatedTaskStatusList]);
+
+  // Change color button SAVE TASK PROGRESS when changes in status where made:
+  const updateStatusButton = document.querySelector(
+    "#save-task-progress",
+  ) as HTMLElement;
+  const taskListAndUpdatedTaskStatusListAreEqual = (
+    taskList: Task[],
+    updatedTaskStatusList: Task[],
+  ) => {
+    if (taskList.length !== updatedTaskStatusList.length) return false;
+
+    for (let i = 0; i < taskList.length; i++) {
+      const taskList1 = taskList[i];
+      const taskList2 = updatedTaskStatusList[i];
+
+      if (
+        taskList1.status !== taskList2.status ||
+        taskList1.percentageCompleted !== taskList2.percentageCompleted
+      ) {
+        return false;
+      }
+    }
+    return true;
+  };
+
+  if (
+    updatedTaskStatusList.length > 0 &&
+    !taskListAndUpdatedTaskStatusListAreEqual(updatedTaskStatusList, taskList)
+  ) {
+    if (updateStatusButton) {
+      updateStatusButton.style.background = "var(--orange)";
+    }
+  } else {
+    if (updateStatusButton) {
+      updateStatusButton.style.background = "var(--null)";
+    }
+  }
 
   // Handle update task progress in the Data Base:
   const handleUpdateTasksStatus = async () => {
@@ -86,48 +117,53 @@ const Tasks = () => {
       </aside>
       <div className="tasks-page">
         <div className="dashboard__main-container">
-          <div className="dashboard__title-welcome">
-            <h2> Hello {userName}</h2>
-            <img
-              src={waving_hand}
-              alt="waving-hand"
-              className="icon__waving-hand"
+          <div className="dashboard__top-info">
+            <div className="dashboard__title-welcome">
+              <h2> Hello {userName}</h2>
+              <img
+                src={waving_hand}
+                alt="waving-hand"
+                className="icon__waving-hand"
+              />
+            </div>
+
+            <h3 className="dashboard__sub-title">Today, {formattedDate}</h3>
+            <div className="dashboard__main-title-and-button">
+              <h2 className="dashboard__main-title"> Tasks:</h2>
+              <button
+                className="button button--primary"
+                id="save-task-progress"
+                onClick={handleUpdateTasksStatus}
+              >
+                Save Task Progress
+              </button>
+            </div>
+            <div className="dashboard__identifiers">
+              <div
+                className="square center_square"
+                style={{
+                  borderColor: "transparent",
+                }}
+              ></div>
+              <h3> Description:</h3>
+              <h3> To do:</h3>
+              <h3>
+                {" "}
+                In progress
+                <br />
+                [% completed]:
+              </h3>
+              <h3> Done:</h3>
+              <img src={menu_vertical} alt="menu_vertical" id="menu_vertical" />
+            </div>
+            {/* Get updated tasks (status and % completed) from TaskList.tsx */}
+          </div>
+          <div className="dashboard__list-container">
+            <TasksList
+              tasks={taskList}
+              onUpdatefromTaskListoTasks={handleUpdateTasksTopParent}
             />
           </div>
-
-          <h3 className="dashboard__sub-title">Today, {formattedDate}</h3>
-          <div className="dashboard__main-title-and-button">
-            <h2 className="dashboard__main-title"> Tasks:</h2>
-            <button
-              className="button button--primary"
-              onClick={handleUpdateTasksStatus}
-            >
-              Save Task Progress
-            </button>
-          </div>
-          <div className="dashboard__identifiers">
-            <div
-              className="square center_square"
-              style={{
-                borderColor: "transparent",
-              }}
-            ></div>
-            <h3> Description:</h3>
-            <h3> To do:</h3>
-            <h3>
-              {" "}
-              In progress
-              <br />
-              [% completed]:
-            </h3>
-            <h3> Done:</h3>
-            <img src={menu_vertical} alt="menu_vertical" id="menu_vertical" />
-          </div>
-          {/* Get updated tasks (status and % completed) from TaskList.tsx */}
-          <TasksList
-            tasks={taskList}
-            onUpdatefromTaskListoTasks={handleUpdateTasksTopParent}
-          />
         </div>
       </div>
     </div>
