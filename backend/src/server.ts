da6d646f087b141c1317d1d1e90a311cea9b0a01
fatimampg <fatimampgouveia@gmail.com>
2024-04-28 12:41:14 +0100

    fix: change cors options in server.ts

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -6,20 +6,24 @@ import { createNewUser, signIn, userName } from "./handlers/user";
 import cors from "cors";
 
 const app = express();
-// const allowedOrigins = process.env.REACT_APP_URL;
-// const options: cors.CorsOptions = {
-//   origin: allowedOrigins,
+
+// const corsOptions = {
+//   origin: process.env.REACT_APP_URL,
+//   optionsSuccessStatus: 200,
 // };
-// app.use(cors(options));
-// app.use(cors());
+// app.use(cors(corsOptions));
 
 app.use(morgan("dev")); //middleware to log requests
 app.use(express.json()); //allow client to send json
 app.use(express.urlencoded({ extended: true }));
 
-app.get("/", (req, res, next) => {
-  res.json({ message: "hello" });
+app.options("*", (req, res) => {
+  res.set("Access-Control-Allow-Origin", process.env.REACT_APP_URL);
+  res.set("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
+  res.set("Access-Control-Allow-Headers", "Content-Type, Authorization");
+  res.set(200).end();
 });
+
 
 app.get("/", (req, res, next) => {
   console.log("hello from express");
@@ -31,19 +35,19 @@ app.use("/api", protect, router); //reject when there is no bearer token
 app.post("/user", createNewUser);
 app.post("/signin", signIn);
 
-app.use((req, res, next) => {
-  res.setHeader(
-    "Access-Control-Allow-Origin",
-    process.env.REACT_APP_URL as string,
-  );
-  res.setHeader(
-    "Access-Control-Allow-Methods",
-    "GET, POST, PUT, DELETE, OPTIONS",
-  );
-  res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
-  res.setHeader("Access-Control-Allow-Credentials", "true");
-  next();
-});
+// app.use((req, res, next) => {
+//   res.setHeader(
+//     "Access-Control-Allow-Origin",
+//     process.env.REACT_APP_URL as string,
+//   );
+//   res.setHeader(
+//     "Access-Control-Allow-Methods",
+//     "GET, POST, PUT, DELETE, OPTIONS",
+//   );
+//   res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
+//   res.setHeader("Access-Control-Allow-Credentials", "true");
+//   next();
+// });
 
 app.use((e: any, req: Request, res: Response, next: NextFunction) => {
   if (e.type === "auth") {
